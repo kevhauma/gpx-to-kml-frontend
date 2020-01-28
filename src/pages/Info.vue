c78664baa2b9a1a5c859f0e314ac06cfbf432825
kevhauma <hauman.kevin@hotmail.com>
2020-01-28 07:06:24 +0100

    broken

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -2,18 +2,20 @@
     <q-page class="infopage">
         <h5>How</h5>
         <p>
-    <ol>
-        <li>Downlaod all your gpx files</li>
-        <li>Install <a href="https://www.google.be/intl/nl_ALL/earth/versions/#download-pro">Google Earth</a></li>
-        <li>Drop all your gpx files here</li>
-        <li>Let the page generate a Google Earth Compatible KML file</li>
-        <li>Download the KML file</li>
-        <li>Open the file in Google Earth</li>
-        <li>Enjoy all your routes</li>
-    </ol>
+            <ol>
+                <li>Downlaod all your gpx files</li>
+                <li>Install <a href="https://www.google.be/intl/nl_ALL/earth/versions/#download-pro">Google Earth</a></li>
+                <li>Drop all your gpx files here</li>
+                <li>Let the page generate a Google Earth Compatible KML file</li>
+                <li>Download the KML file</li>
+                <li>Open the file in Google Earth</li>
+                <li>Enjoy all your routes</li>
+            </ol>
         </p>
         <h5>Why</h5>
         <p>Because importing GPX files into google earth is cumbersome. Routes are nested in multiple folder, submenu is hard to find,...</p>
+       <h5>It broke</h5>
+        <p>it worked on my machine. If it doesn't on yours, post an issue on github</p>
         <h5>What</h5>
         <p>This web page is made with VueJs and Quasar framework, GPX parser is with the <a href="https://www.npmjs.com/package/gpx-parse">gpx-parse</a> npm package</p>
         <h5>Open-source</h5>
@@ -25,7 +27,8 @@
     </q-page>
 </template>
 <style>
-    .infopage{
+    .infopage {
         margin: 20px;
     }
+
 </style>
